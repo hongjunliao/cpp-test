708e76d7910f8a0574bcb15f0dde167dd69e50b5
hongjun.liao <docici@126.com>
2017-09-30 18:00:10 +0800

    src/tests/test_socks.cpp

---
+++ src/tests/test_socks.cpp
@@ -0,0 +1,11 @@
+/*!
+ * test for libsocksd0-dev,
+ * (1)SOCKS protocol impl, see dante: http://www.inet.no/dante/
+ * (2)https://en.wikipedia.org/wiki/SOCKS#SOCKS_proxy_server_implementations
+ * */
+#include <socks.h>
+
+int test_socks_main(int argc, char ** argv)
+{
+	return 0;
+}
