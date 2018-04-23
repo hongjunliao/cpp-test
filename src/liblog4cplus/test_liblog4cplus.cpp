3f3c4f7f87d4fe4f9fac6e75ead3ad249f1db394
hongjun.liao <docici@126.com>
2018-04-23 11:34:52 +0800

    update test: liglog4cplus

--- src/liblog4cplus/test_liblog4cplus.cpp
+++ src/liblog4cplus/test_liblog4cplus.cpp
@@ -18,6 +18,7 @@
 #include <assert.h>     /* define NDEBUG to disable assertion */
 
 #include <log4cplus/consoleappender.h>
+#include <log4cplus/fileappender.h>
 #include <log4cplus/layout.h>
 #include <log4cplus/logger.h>
 #include <log4cplus/ndc.h>
@@ -31,27 +32,71 @@
 #include "log4cplus/consoleappender.h"
 #include "log4cplus/loglevel.h"
 #include <log4cplus/loggingmacros.h>
+//#include <log4cplus/helpers/loglog.h>
 #include <exception>
 #include <iostream>
 #include <string>
 
 using namespace std;
 using namespace log4cplus;
+using namespace log4cplus::helpers;
+
+Logger * glog4logger = 0;
+Logger glog4logger2 = Logger::getInstance(("testliblog4clplus"));
+
+/* cant work!
+ * undefined symbol: _ZN9log4cplus6LoggerC1EOS0_ */
+//#define log4info(args...) LOG4CPLUS_INFO_FMT(Logger::getRoot(), ##args)
+//#define log4info0(args...) LOG4CPLUS_INFO_FMT(Logger::getInstance(("testliblog4clplus")), ##args)
+
+#define log4inf1(args...) LOG4CPLUS_INFO_FMT(*glog4logger, ##args)
+#define log4info2(fmt, args...) LOG4CPLUS_INFO_FMT(*glog4logger, fmt, ##args)
+#define log4info3(fmt, args...) LOG4CPLUS_INFO_FMT(glog4logger2, fmt, ##args)
 
 int test_liblog4cplus_main(int argc, char ** argv)
 {
     log4cplus::initialize ();
     std::auto_ptr<log4cplus::Layout> layout(new log4cplus::PatternLayout("[%D,%d{%q}][%p] [%l] %m%n"));
     SharedAppenderPtr append_1(new ConsoleAppender());
-    append_1->setName(LOG4CPLUS_TEXT("testliblog4clplus"));
+    append_1->setName(("testliblog4clplus"));
     append_1->setLayout(layout);
     Logger::getRoot().addAppender(append_1);
-    Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("test"));
+
+    SharedAppenderPtr append_2(
+        new RollingFileAppender(LOG4CPLUS_TEXT("/tmp/testliblog4clplus.log"), 5*1024, 5));
+    append_2->setName(LOG4CPLUS_TEXT("testliblog4clplus1"));
+    append_2->setLayout( std::auto_ptr<Layout>(new TTCCLayout()) );
+    Logger::getRoot().addAppender(append_2);
+
+    Logger logger = Logger::getInstance(("testliblog4clplus"));
 
     LOG4CPLUS_INFO_FMT(logger,
                     LOG4CPLUS_TEXT (
                         "a log from log4cplus.%d, %d, %d, %ls...DONE"),
                     1, 2, 3, L"testing");
+
+    LOG4CPLUS_INFO_FMT(logger, "another log from log4cplus, str='%s'", "hello");
+    LogLog::getLogLog()->warn("a warn msg");
+
+    Logger logger2 = Logger::getInstance(("testliblog4clplus"));
+    glog4logger = &logger2;
+    LOG4CPLUS_INFO_FMT(*glog4logger,
+                        LOG4CPLUS_TEXT (
+                            "LOG4CPLUS_INFO_FMT.%d, %d, %d, %ls...DONE"),
+                        1, 2, 3, L"testing");
+
+    log4inf1("log4inf1: fuck log4cplus, str='%s'", "fuck");
+
+    /* fuck cant work! */
+//    log4info("fuck log4cplus");
+//    log4info2("fuck log4cplus");
+//    log4info0("fuck log4cplus, str='%s'", "fuck");
+
+    log4info2("log4info2: fuck log4cplus, str='%s'", "fuck");
+    log4info3("log4info3: fuck log4cplus, str='%s'", "fuck");
+
+
+    log4cplus::threadCleanup ();
     return 0;
 }
 
