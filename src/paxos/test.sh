a89b52e59d3a91fc7db3b7cd366dcf70ee9cb6c5
hongjun.liao <docici@126.com>
2018-07-11 19:49:57 +0800

    test_paxos.c; change nodes to 99

--- src/paxos/test.sh
+++ src/paxos/test.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
+# This file is PART of cpp-test project
+# @author hongjun.liao <docici@126.com>, @date 2018/7/11
+# 
+# test paxos protocol
+# https://github.com/Tencent/phxpaxos
+
 arg_n='';
 arg_a='';
 args='';
-for i in {1..21}; do
+for i in {1..99}; do
 #	echo $((i+1));
-	arg_n+=",127.0.0.1:"$((i+11110));
+	arg_n+=",127.0.0.1:"$((i+11100));
 done;
 	arg_n=${arg_n:1};
 #	echo $arg_n;
 
-for i in {1..21}; do
-	arg_a="127.0.0.1:"$((i+11110));
-	args='paxos1 -i'$((i+11110))' -a'${arg_a}' -n'${arg_n}' -v6 -f /home/jun/sdb1/cpp-test/src/paxos/.echo_file';
-#	echo "$((i+11110)) hello" >> .echo_file
+for i in {1..99}; do
+	arg_a="127.0.0.1:"$((i+11100));
+	args='paxos1 -i'$((i+11100))' -a'${arg_a}' -n'${arg_n}' -v6 -f /home/jun/sdb1/cpp-test/src/paxos/.echo_file';
+	echo "$((i+11100)) $((i+11100))" >> .echo_file
 #	echo $args;
 	cpp-test-main $args > /tmp/paxos_$i&
 done;
